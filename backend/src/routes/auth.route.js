b5aa1ee74a1cbf9bc829a08e7ca374154c30d023
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-09-17 21:12:36 +0530

    initial commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+  res.send("Sign-Up");
+});
+
+router.get("/login", (req, res) => {
+  res.send("Login");
+});
+
+export default router;
